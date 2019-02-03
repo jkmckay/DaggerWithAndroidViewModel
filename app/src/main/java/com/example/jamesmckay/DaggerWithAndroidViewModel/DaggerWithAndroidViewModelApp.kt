13038736d5a88e8f74629f517e4e74b811ae491c
James <james.mckay133@gmail.com>
2019-02-04 09:25:56 +1100

    Finalising fragment injection.

--- app/src/main/java/com/example/jamesmckay/DaggerWithAndroidViewModel/DaggerWithAndroidViewModelApp.kt
+++ app/src/main/java/com/example/jamesmckay/DaggerWithAndroidViewModel/DaggerWithAndroidViewModelApp.kt
@@ -3,10 +3,15 @@ package com.example.jamesmckay.DaggerWithAndroidViewModel
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.jamesmckay.DaggerWithAndroidViewModel.DI.components.DaggerAppComponent
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
 class DaggerWithAndroidViewModelApp : Application(), HasActivityInjector {
@@ -19,6 +24,7 @@ class DaggerWithAndroidViewModelApp : Application(), HasActivityInjector {
         super.onCreate()
 
     }
+
     //dispatchingAndroidInjector for the app's activities.
     override fun activityInjector(): DispatchingAndroidInjector<Activity> {
         return dispatchingAndroidInjector
@@ -33,12 +39,24 @@ class DaggerWithAndroidViewModelApp : Application(), HasActivityInjector {
             .inject(this)
 
         registerActivityLifecycleCallbacks(object : AbstractActivityLifecycleCallbacks() {
-            //        on activity created inject this activity with its dependencies
+            //       when 'onActivityCreated' is called; inject this activity with its dependencies
             override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
-      //                if (activity is HasSupportFragmentInjector) {
+                /*
+                Check to see if the Activity has implemented HasSupportFragmentInjector so that any fragments
+                may have their dependencies provided. Although this check is unnecessary for this project at this stage,
+                it is here purely as an example.
+                 */
+                if (activity is HasSupportFragmentInjector) {
                     AndroidInjection.inject(activity)
-
-//                }
+                }
+                //injects fragment in question's dependencies when onFragmentCreated is called.
+                (activity as AppCompatActivity).supportFragmentManager.registerFragmentLifecycleCallbacks(
+                    object : FragmentManager.FragmentLifecycleCallbacks() {
+                        override fun onFragmentCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
+                                AndroidSupportInjection.inject(f)
+                        }
+                    }, true
+                )
 
             }
         })
